1055f061be15eaff89a82ae3a7a4728cdf72298e
bentobox7 <105513956+bentobox7@users.noreply.github.com>
2025-10-08 06:44:49 +0000

    modal html css

---
+++ szoleszborasz.js
@@ -0,0 +1,17 @@
+var f = document.getElementById("feher");
+var r = document.getElementById("rose");
+var v = document.getElementById("voros");
+var fb = document.getElementById("feherb");
+var rb = document.getElementById("roseb");
+var vb = document.getElementById("vorosb");
+var bezar = document.getElementsByClassName("bezar")[0];
+
+fb.onclick = function() {
+  f.style.display = "block";
+}
+rb.onclick = function() {
+  r.style.display = "block";
+}
+vb.onclick = function() {
+  v.style.display = "block";
+}
