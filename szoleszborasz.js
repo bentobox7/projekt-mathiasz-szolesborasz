fb08684d48b02e56abca392b9fa060b5274295bc
toth_bence <toth.bence.th@gmail.com>
2025-10-10 11:30:07 +0200

    KARDOS kepek

--- szoleszborasz.js
+++ szoleszborasz.js
@@ -1,13 +1,15 @@
-let feherkepek = []
+let feherkepek = ["csomosz.jpg","erjesztes.jpg","erleles_f.jpg","presles.jpg","preseles2.jpg","szolpres.jpg","szuret_f.jpg","szuret_r.jpg","ulepites_f.jpg"]
 let feherh2 = ["Szőlőszüret","Zúzás és préselés","Ülepítés","Erjesztés", "Érlelés", "Szűrés és palackozás"]
 let feherp = ["Általában zöld vagy sárga héjú szőlőt használnak.", "A szőlőt azonnal kipréselik, hogy a héjból ne oldódjon ki szín vagy fanyar anyag.", "A mustot hagyják leülepedni, hogy a zavaros részecskék eltávolíthatók legyenek.", " A tiszta mustot alacsony hőmérsékleten, rozsdamentes acéltartályokban erjesztik, így megőrzik a friss, gyümölcsös aromákat.", "Rövidebb ideig tart, gyakran tartályban, ritkábban hordóban.", "A bort letisztítják, majd palackozzák."]
-let rosekepek = []
+let rosekepek = ["csomosz.jpg","erjesztes.jpg","erleles_f.jpg","presles.jpg","preseles2.jpg","szolpres.jpg","szuret_f.jpg","szuret_r.jpg","ulepites_f.jpg"]
 let roseh2 = ["Szőlőszüret", "Zúzás", "Rövid héjon áztatás", "Préselés","Erjesztés","Érlelés és palackozás"]
 let rosep = ["Kék szőlőfajtákból készül.", "A szőlőszemeket enyhén zúzzák.", "A héjjal együtt hagyják állni 6–24 óráig (szemben a vörösbor hosszabb héjon erjesztésével), hogy csak enyhe szín és kevés tannin oldódjon ki.", "Az áztatás után a levet elválasztják a héjtól.","Hasonló módon történik, mint a fehérboroknál, alacsony hőmérsékleten.", "Gyorsabban piacra kerül, friss, üde stílusban."]
-let voroskepek = []
+let voroskepek = ["csomosz.jpg","erjesztes.jpg","erleles_f.jpg","presles.jpg","preseles2.jpg","szolpres.jpg","szuret_f.jpg","szuret_r.jpg","ulepites_f.jpg"]
 let vorosh2 = ["Szőlőszüret", "Zúzás és bogyózás", "Erjesztés héjjal együtt", "Csömöszölés", "Préselés", "Érlelés", "Szűrés és palackozás"]
 let vorosp = ["Érett, kék szőlőt szüretelnek.", "A szőlőszemeket összezúzzák, leválasztják a szárakat.", "A cefrét (zúzott szőlő héjjal, maggal) tartályba töltik, és természetes vagy hozzáadott élesztővel erjesztik. A héj jelenléte biztosítja a bor sötét színét és a tanninokat, amelyek a bor szerkezetéért és érlelhetőségéért felelősek.", "Az erjedés alatt a héjat folyamatosan visszanyomják a lébe, hogy jobb szín- és ízanyag-kioldódást érjenek el.", "Az erjedés után a bort lefejtik, a visszamaradt héjat és magot kipréselik.", "A bort fahordóban vagy rozsdamentes tartályban érlelik több hónapig, akár évekig is.", " A bort letisztítják, majd palackozzák."]
 
+let kerdesek = ["Melyik borvidék híres a Tokaji aszúról?","Melyik NEM fehérszőlő fajta?","Mit jelent a „cuvée”?","Melyik bor típikusan hosszabban érlelhető?","Miért nem ajánlott túl hidegen felszolgálni a vörösbort?"]
+let valaszok = []
 
 let i = 0;
 
@@ -58,6 +60,9 @@ function FeltoltV() {
 
 }
 
+function Quiz(){
+  
+}
 
 function submitQuiz() {
   const form = document.getElementById('quizForm');
@@ -71,11 +76,11 @@ function submitQuiz() {
 
   let message = "";
   if (score === 5) {
-    message = "🏅 Gratulálunk! Valódi borszakértő vagy!";
+    message = "Gratulálunk! Valódi borszakértő vagy!";
   } else if (score >= 3) {
-    message = "🍇 Szép munka! Jó úton jársz, hogy borszakértő legyél.";
+    message = "Szép munka! Jó úton jársz, hogy borszakértő legyél.";
   } else {
-    message = "🍷 Még van mit tanulni – irány egy borkóstoló!";
+    message = "Még van mit tanulni – irány egy borkóstoló!";
   }
 
   result.textContent = `Eredményed: ${score}/5 – ${message}`;
